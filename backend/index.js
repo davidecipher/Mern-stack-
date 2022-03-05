a3679bf0326d04c8ebc1fd3ac6d9237e31268d75
David Decipher <davidecipher@gmail.com>
2022-03-05 09:03:35 +0100

    Added routes, models and controllers

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use(cors);
+app.use(cors());
+
+app.use('/api', require('./routes/postRoutes'));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
    console.log('Database Connected')   
